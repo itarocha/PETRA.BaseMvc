9fa75c0d3cdddbd7618d23bac44217e9095df06e
itarocha <itarocha@gmail.com>
2014-09-11 10:38:14 -0300

    Manter Case

---
+++ Petra.Util/Attributes/ManterCaseAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petra.Util.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class ManterCaseAttribute : System.Attribute
+    {
+        public ManterCaseAttribute()
+        {
+        }
+        /*
+        public string NomeEntidade { get; set; }
+        public string Objeto { get; set; }
+        public string Campo { get; set; }
+        // "Dardani.EDU.BO.NH.SalaDAO"
+        public string ClasseDao { get; set; }
+        //"Dardani.EDU.BO.dll";
+        public string FileName { get; set; }
+         */ 
+    }
+}
--- Petra.Util/Funcoes/Criptografia.cs
+++ Petra.Util/Funcoes/Criptografia.cs
@@ -11,7 +11,7 @@ namespace Petra.Util.Funcoes
         public static string MD5(string input)
         {
             System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
-            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
             byte[] hash = md5.ComputeHash(inputBytes);
             System.Text.StringBuilder sb = new System.Text.StringBuilder();
             for (int i = 0; i < hash.Length; i++)
